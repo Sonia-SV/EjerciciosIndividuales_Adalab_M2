82616344d1f9c3c7fe92ba9b22b4cf8cca329123
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-06-28 17:03:29 +0200

    Finish weekend fun

--- Weekend Fun/app/js/main.js
+++ Weekend Fun/app/js/main.js
@@ -1,21 +1,45 @@
 'use strict';
 
 //SELECTORS...
+const button = document.querySelector('.js-login-button');
+const warning = document.querySelector('.error-wrapper');
+const remaining = document.querySelector('.countdown');
+const userInputName = document.querySelector('.js-user-name');
+const userInputPassword = document.querySelector('.js-user-password');
 
 //Number of user attemps
 let attemps = 0;
-
 //Object saved in BBDD with user information to compare with my form
 const userInfo = {
-	name: 'front23',
-	password: 'bicicleta'
+  name: 'front23',
+  password: 'bicicleta',
 };
 
 //FUNCTIONS
-
-
-
-
-
+function login() {
+  if (
+    userInfo.name === userInputName.value &&
+    userInfo.password === userInputPassword.value
+  ) {
+    warning.classList.add('hidden');
+    remaining.classList.add('hidden');
+    return alert('Estás dentro');
+  } else if (userInputName.value === '' || userInputPassword.value === '') {
+    return alert('Introduce tu usuario y contraseña');
+  } else if (attemps < 3) {
+    attemps++;
+    console.log(`llevas ${attemps} intentos`);
+    remaining.innerHTML = `Te quedan ${4 - attemps} intentos.`;
+    return (
+      warning.classList.remove('hidden') + remaining.classList.remove('hidden')
+    );
+  } else {
+    warning.classList.add('hidden');
+    remaining.innerHTML = `No te quedan intentos.`;
+    return alert('Estás bloqueado');
+  }
+}
 
 //LISTENERS
+
+button.addEventListener('click', login);
